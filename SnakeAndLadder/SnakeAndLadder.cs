d2653f739c64729772a2be3d93e6a47a24a644d9
priyanshu-rathore <priyanshurathore132@gmail.com>
2022-12-21 13:30:02 +0530

    Initialize player at postion 0

---
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder
+    {
+        public static void snakeAndLadder()
+        {
+            String Player = "Priyanshu";
+            int position = 0;
+            Console.WriteLine("Player {0} is at postion {1}",Player,position);
+        }
+    }
+}
